962bd6a7072770810a7aa01c83d7ed79b983b3d6
Zafrul Hasan Khan (Nasim) <nasimkhan18003@gmail.com>
2020-02-26 21:41:24 +0600

    File added

---
+++ delete position.cpp
@@ -0,0 +1,23 @@
+//A program for delete array element
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[6],n,i,j,m ;
+    int temp;
+    cin>>n;
+    cout<<"Enter the array elements:";
+    for(i=0;i<n;i++)
+        cin>>a[i];
+    cout<<"Enter the delete position:";
+    cin>>m;
+     for(i=m-1;i<n-1;i++)
+     {
+         a[i]=a[i+1];
+     }
+     cout<<"array elements after deletion:";
+     for(i=0;i<n-1;i++)
+     cout<<a[i]<<" ";
+     return 0;
+}
+
